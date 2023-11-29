66b6eaf708cb78abd253e2c074f4c7c8f33ad896
Mahir Mujkanovic <mahir.mujkanovic@outlook.com>
2023-11-29 09:45:17 -0800

    Create GetRemoteSessions.ps1

---
+++ GetRemoteSessions.ps1
@@ -0,0 +1,16 @@
+﻿$quserResult = New-Object -TypeName 'System.Collections.ArrayList';
+$queryAllConnections=(((quser) -replace '^>', '') -replace '\s{2,}', ',').Trim() | ForEach-Object {
+    if($_.Split(',').Count -eq 5) {
+        Write-Output ($_-replace '(^[^,]+)', '$1,')
+    } else{
+        Write-Output $_}
+} | ConvertFrom-Csv
+
+foreach($userSession in $queryAllConnections)
+{
+    if($userSession.SESSIONNAME -like "rdp*"){
+         $quserResult.Add($userSession) >$null
+    }
+        
+    
+}
